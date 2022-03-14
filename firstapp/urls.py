03e0f47d53b8f1481aa0d02be1c6d5476d26d107
RafsanRifat <rafsan.rifat255@gmail.com>
2022-03-14 11:42:37 +0600

    registration api working

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import firstAPI
+from .views import firstAPI, registrstionAPI
 
 urlpatterns = [
     path('first/', firstAPI, name="api"),
+    path('registration/', registrstionAPI, name="registration"),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -20,3 +21,31 @@ def firstAPI(request):
             'age': 500
         }
     return Response(context)
+
+
+# Registration
+
+@api_view(['POST'])
+def registrstionAPI(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        password1 = request.data['password1']
+        password2 = request.data['password2']
+        email = request.data['email']
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username is already exists'})
+        if password1 != password2:
+            return Response({'error': 'Password did not match '})
+        else:
+            user = User()
+            user.username = username
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.is_active = True
+            user.set_password(raw_password=password1)
+            user.save()
+            return Response({'success': 'User is created successfully'})
